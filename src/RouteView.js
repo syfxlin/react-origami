dd5144795fb20852c90c47eebfa86a646425b420
syfxlin <syfxlin@gmail.com>
2019-11-25 16:26:50 +0800

    feat: :sparkles: Add route view

---
+++ src/RouteView.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+export default function RouteView(props) {
+  let alias = [];
+  return (
+    <Switch>
+      {props.route.map(item => {
+        if (item.alias) {
+          alias.push({
+            from: item.alias,
+            to: item.path
+          });
+        }
+        return (
+          <Route
+            path={item.path}
+            component={item.component}
+            key={item.path}
+            exact={item.path === '/'}
+          />
+        );
+      })}
+      {alias.map(item => (
+        <Redirect from={item.from} to={item.to} />
+      ))}
+    </Switch>
+  );
+}
